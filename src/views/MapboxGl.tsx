d4f32b5d75d55858516c72d3032442217c7c4600
chanhenghong <chanheng2002@gmail.com>
2022-09-06 11:44:14 +0700

    update

--- src/views/MapboxGl.tsx
+++ src/views/MapboxGl.tsx
@@ -8,16 +8,18 @@ const Map = ReactMapboxGl({
 const Mapbox = () => {
   // const optCenter: any = [104.9038566, 11.5774552];
   // const optZoom = 13;
-  <Map
-    style="mapbox://styles/mapbox/streets-v9"
-    containerStyle={{
-      height: "100vh",
-      width: "100vw",
-    }}
-  >
-    <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-      <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-    </Layer>
-  </Map>;
+  return (
+    <Map
+      style="mapbox://styles/mapbox/streets-v9"
+      containerStyle={{
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+      </Layer>
+    </Map>
+  );
 };
 export default Mapbox;
